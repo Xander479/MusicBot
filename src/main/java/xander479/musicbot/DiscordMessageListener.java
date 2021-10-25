8d380c6538f9e1e3e033c9d1346db710c065a8b5
Xander <contact@xander479.co.uk>
2021-10-26 00:10:02 +0100

    Initial commit. Allow joining/leaving of voice channels

---
+++ src/main/java/xander479/musicbot/DiscordMessageListener.java
@@ -0,0 +1,58 @@
+package xander479.musicbot;
+
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
+import org.javacord.api.entity.channel.ServerTextChannel;
+import org.javacord.api.entity.channel.ServerVoiceChannel;
+import org.javacord.api.entity.message.MessageAuthor;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.listener.message.MessageCreateListener;
+
+public class DiscordMessageListener implements MessageCreateListener {
+	private MusicBot.BotProperties properties;
+	
+	@Override
+	public void onMessageCreate(MessageCreateEvent event) {
+		String prefix = properties.getPrefix();
+		String msg = event.getMessageContent();
+		if(!msg.startsWith(prefix)) return;
+		
+		String[] words = msg.split(" ");
+		ServerTextChannel channel = event.getServerTextChannel().get();
+		MessageAuthor author = event.getMessageAuthor();
+		Server server = event.getServer().get();
+		
+		// checking command name
+		switch(words[0].substring(prefix.length())) {
+			case "join":
+				channel.sendMessage("Joining " + channel.getName());
+				Optional<ServerVoiceChannel> vc = author.getConnectedVoiceChannel();
+				if(vc.isPresent()) {
+					try {
+						properties.setAudioConnection(vc.get().connect().get());
+					}
+					catch (InterruptedException | ExecutionException e) {
+						channel.sendMessage("Couldn't join voice channel");
+						e.printStackTrace();
+					}
+				}
+				else channel.sendMessage("You must be connected to a voice channel to use this command.");
+				break;
+				
+			case "leave":
+				vc = MusicBot.getInstance().getApi().getYourself().getConnectedVoiceChannel(server);
+				properties.getAudioConnection().close();
+				properties.setAudioConnection(null);
+				break;
+			
+			case "disconnect":
+				MusicBot.getInstance().disconnect();
+		}
+	}
+	
+	public void setProperties(MusicBot.BotProperties properties) {
+		this.properties = properties;
+	}
+}
---
+++ src/main/java/xander479/musicbot/MusicBot.java
@@ -0,0 +1,107 @@
+package xander479.musicbot;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.audio.AudioConnection;
+
+public class MusicBot {
+	private static MusicBot instance;
+	private final String token;
+	private DiscordApi api;
+	private BotProperties properties;
+
+	public static void main(String[] args) {
+		getInstance();
+	}
+	
+	private MusicBot() {
+		token = "Your Discord's Bot Token Here";
+		api = new DiscordApiBuilder().setToken(token).login().join();
+		api.addMessageCreateListener(new DiscordMessageListener());
+		try {
+			properties = BotProperties.getProperties();
+		}
+		catch(IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+			properties = BotProperties.initialise();
+		}
+		// Set properties now, so the constructors don't enter an infinite loop
+		((DiscordMessageListener)api.getMessageCreateListeners().get(0)).setProperties(properties);
+	}
+	
+	public static synchronized MusicBot getInstance() {
+		if(instance == null) instance = new MusicBot();
+		return instance;
+	}
+	
+	public BotProperties getProperties() {
+		return properties;
+	}
+	
+	public DiscordApi getApi() {
+		return api;
+	}
+	
+	public void disconnect() {
+		api.disconnect();
+	}
+	
+	static class BotProperties implements Serializable {
+		private static final long serialVersionUID = 1L;
+		private String prefix = "!";
+		private AudioConnection auCon;
+		
+		private BotProperties() {
+			prefix = "!";
+			try {
+				saveProperties();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		private static BotProperties initialise() {
+			return new BotProperties();
+		}
+		
+		private static BotProperties getProperties() throws FileNotFoundException, IOException, ClassNotFoundException {
+			try(ObjectInputStream in = new ObjectInputStream(new FileInputStream("bot.properties"))) {
+				Object properties = in.readObject();
+				if(properties instanceof BotProperties) return (BotProperties)properties;
+			}
+			throw new IOException("Unable to deserialise properties");
+		}
+		
+		public void saveProperties() throws FileNotFoundException, IOException {
+			try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("bot.properties"))) {
+				out.writeObject(this);
+			}
+		}
+		
+		public String getPrefix() {
+			return prefix;
+		}
+		
+		public void setPrefix(String prefix) {
+			this.prefix = prefix;
+		}
+		
+		public AudioConnection getAudioConnection() {
+			return auCon;
+		}
+		
+		public void setAudioConnection(AudioConnection auCon) {
+			this.auCon = auCon;
+		}
+	}
+}
